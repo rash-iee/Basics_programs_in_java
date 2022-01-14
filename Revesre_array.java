13fe2c1c03c73e3b44fea1ab260561079658a636
Rashi Baheti <69914332+rash-iee@users.noreply.github.com>
2022-01-14 19:24:01 +0530

    Add files via upload

---
+++ Revesre_array.java
@@ -0,0 +1,28 @@
+package basics_of_OOps;
+
+public class Revesre_array {
+	static void reverse(int arr[], int size) {
+		int temp;
+		int i;
+		for(i=0;i< size/2 ;i++) {    // moving till half size of the array adn swapping the last and first indexed values and doning this untill we reacg middle 
+			 temp = arr[i];
+			 arr[i] = arr[size-i-1];
+			 arr[size-i-1] = temp;
+			 
+		}
+		System.out.println("Revesred array is ");
+		for(int j=0;j<size;j++) {
+			System.out.println( arr[j]);
+		}
+		
+	}
+	
+
+	
+	
+	public static void main(String[] args) {
+		
+		int[] arr = { 1, 2, 3, 4, 5 };
+        reverse(arr, arr.length);
+}
+}
